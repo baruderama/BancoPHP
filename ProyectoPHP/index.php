74e7e385efd0b4224b667143fb319f9503ce28d8
Brayan Ricardo Garcia Morales <brayangarcia44@hotmail.com>
2019-11-15 23:39:35 -0500

    Login y Registro terminados, falta tabla productos

--- ProyectoPHP/index.php
+++ ProyectoPHP/index.php
@@ -1,22 +1,137 @@
 <?php 
-	if(isset($_POST['btnOpBan']))
+	include_once dirname(__FILE__).'/Conf/config.php';
+					$con=mysqli_connect(HOST_DB,USUARIO_DB,USUARIO_PASS,NOMBRE_DB);
+	if(isset($_POST['btnProductos']))
 	{
-		
+		$str_datos.='<table border="1" style="width:100%">';
+		$str_datos.='<tr>';
+		$str_datos.='<th>Producto</th>';
+		$str_datos.='</tr>';
+		$sql = " select* from productos";
+		$resultado = mysqli_query($con,$sql);
+		while($fila = mysqli_fetch_array($resultado)) {
+			$str_datos.='<tr>';
+			$str_datos.= "<td>".$fila['nombre']."</td>";
+			$str_datos.= "</tr>";
+		}
+		$str_datos.= "</table>";
+		echo $str_datos;
 	}
 	else
 	{
 		if(isset($_POST['btnIniSes']))
 		{
-			
+			echo "<form action='' method='post'>
+					<input type='text' placeholder='Login Nickname' name = 'txtLogNom'>
+					<input type='password' placeholder='Password' name = 'txtLogPass'>
+					<input type='submit' value='Iniciar Sesion' name = 'btnLogin'>
+				</form>";
 		}
 		else
 		{
-			if(isset($_POST['btnOpBan']))
+			if(isset($_POST['btnReg']))
 			{
-				
+				echo "<form action='' method='post'>
+					<input type='text' placeholder='Nickname' name = 'txtRegNom'>
+					<input type='password' placeholder='Password' name = 'txtRegPass'>
+					<input type='submit' value='Registrarse' name = 'btnRegistrar'>
+				</form>";
+			}
+			else
+			{
+				if(isset($_POST['btnRegistrar']))
+				{
+					echo "ENTRA";
+					$nombre = $_POST['txtRegNom'];
+					$sql = "SELECT COUNT(1) FROM administrador where usuario = '$nombre' ";
+					$aux = mysqli_fetch_row(mysqli_query($con,$sql));
+					$existe = false;
+					if($aux[0]==1)
+					{
+						echo "Este nickname ya existe";
+						$existe = true;
+					}
+					else
+					{
+						$sql = "SELECT COUNT(1) FROM clientes where usuario = '$nombre' ";
+						$aux = mysqli_fetch_row(mysqli_query($con,$sql));
+						if($aux[0]==1)
+						{
+							echo "Este nickname ya existe";
+							$existe = true;
+						}
+					}
+					if(!$existe)
+					{
+						$password = $_POST['txtRegPass'];
+						$passCrypt = crypt('$password','$6iahEstoyArto');
+						$sql="SELECT COUNT(PID) FROM administrador";
+						$aux = mysqli_fetch_row(mysqli_query($con,$sql));
+						if($aux[0]==0)
+						{
+							$sql = "INSERT INTO administrador(USUARIO, CONTRASENHA) VALUES ('$nombre','$passCrypt')";
+							if(mysqli_query($con,$sql))
+							{
+								echo "El administrador $nombre ha sido creado correctamente";
+							}
+							else{
+								echo "Error en la creación:".mysqli_error($con).".<br>";
+							}
+						}
+						else
+						{
+							$sql = "INSERT INTO clientes (USUARIO, CONTRASENHA) VALUES ('$nombre','$passCrypt')";
+							if(mysqli_query($con,$sql))
+							{
+								echo "El cliente $nombre ha sido creado correctamente";
+							}
+							else{
+								echo "Error en la creación".mysqli_error($con).".<br>";
+							}
+						}
+					}
+					
+					
+				}
+				else
+				{
+					if(isset($_POST['btnLogin']))
+					{
+						$nombre = $_POST['txtLogNom'];
+						$password = $_POST['txtLogPass'];
+						$passCrypt = crypt('$password','$6iahEstoyArto');
+						$sql = "SELECT COUNT(1) FROM administrador where usuario = '$nombre' and contrasenha = '$passCrypt' ";
+						$aux = mysqli_fetch_row(mysqli_query($con,$sql));
+						if($aux[0]==1)
+						{
+							echo "<h2>Bienvenido $nombre </h2>";
+							session_start();
+							$_SESSION["newsession"]=$nombre;
+							echo $_SESSION["newsession"];
+						}
+						else
+						{
+							$sql = "SELECT COUNT(1) FROM clientes where usuario = '$nombre' and contrasenha = '$passCrypt' ";
+							$aux = mysqli_fetch_row(mysqli_query($con,$sql));
+							if($aux[0]==1)
+							{
+								echo "<h2>Bienvenido $nombre </h2>";
+								session_start();
+								$_SESSION["newsession"]=$nombre;
+								echo $_SESSION["newsession"];
+							}
+							else
+							{
+								echo "Nickname o password incorrectos";
+							}
+						}
+					}
+				}
 			}
 		}
+		mysqli_close($con);
 	}
+	
 ?>
 <html>
 	<head>
@@ -26,13 +141,11 @@
 		<form action="CrearTablas.php" method="post">
 			<input type="submit" value="Crear Tablas">
 		</form>
-		<form action="<?php ?>" method="post">
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
 			<input type="submit" value="Registrarse" name = "btnReg">
-		</form>
 			<input type="submit" value="Iniciar Sesion" name = "btnIniSes">
-			<form action="OperacionesBancarias.php" method="post">
-			<input type="submit" value="Operacion Bancaria" name = "btnOpBan">
-			</form>
+			<input type="submit" value="Buscar Producto" name = "btnProductos">
+		</form>
 	</body>
 </html>
 
