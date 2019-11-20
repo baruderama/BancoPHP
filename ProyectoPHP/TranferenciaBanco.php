f51b469c2bb24e670820ddae7358f945c9f044b7
veraele <vera_l@javeriana.edu.co>
2019-11-20 11:31:59 -0500

    Medio Terminado

--- ProyectoPHP/TranferenciaBanco.php
+++ ProyectoPHP/TranferenciaBanco.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Transferencia Banco</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+//aquí lo de bancos 
+// esta complicado
+
+
+?>
